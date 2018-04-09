33612c43fe164edc814b7666b8c422706db34102
LeonardoSá <32676285+leodesa@users.noreply.github.com>
2018-04-09 10:15:38 -0300

    Validação parcial

--- js/init.js
+++ js/init.js
@@ -5,3 +5,22 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function validar(){
+	usuario = form1.usuario.value;
+	verificarVazio(usuario);
+	senha = form1.password.value;
+	verificarVazio(senha);
+	confSenha = form1.passwordConf.value;
+	verificarVazio(confSenha);
+
+	if (senha != confSenha){
+		alert('Senhas Diferentes!')
+	}
+}
+
+function verificarVazio(campo){
+	if (campo == ''){
+		alert('Campos com "*" devem ser preenchidos')
+	}
+}
