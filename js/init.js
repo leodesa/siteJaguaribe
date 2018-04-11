092cef14da5de7e73acb8a3daacd033932b55b99
LeonardoSá <32676285+leodesa@users.noreply.github.com>
2018-04-11 19:35:50 -0300

    Validação de Dados

--- js/init.js
+++ js/init.js
@@ -10,8 +10,19 @@ function validar(){
 	usuario = cadastro.usuario.value;
 	senha = cadastro.password.value;
 	confSenha = cadastro.passwordConf.value;
+	rasaoSocial = cadastro.rasaoSocial.value;
+	cnpj = cadastro.cnpj.value;
+	rua = cadastro.rua.value;
+	numeroCasa = cadastro.numeroCasa.value;
+	bairro = cadastro.bairro.value;
+	cidade = cadastro.cidade.value;
+	telefone = cadastro.telefone.value;
+	uf = cadastro.uf.value;
 
-	if (verificarVazio(usuario) || verificarVazio(senha) || verificarVazio(confSenha)) {
+	if (verificarVazio(usuario) || verificarVazio(senha) || verificarVazio(confSenha)
+	 || verificarVazio(rasaoSocial) || verificarVazio(cnpj) || verificarVazio(rua)
+	 || verificarVazio(numeroCasa) || verificarVazio(bairro) || verificarVazio(cidade)
+	 || verificarVazio(telefone) || verificarVazio(uf)){
 		alert('Todos os capos com "*" devem ser preenchidos.')
 	}
 
