aad33ac8f830becb3b0feb431f803c3d488694ab
LeonardoSá <32676285+leodesa@users.noreply.github.com>
2018-04-14 10:29:33 -0300

    ç8oli.7li7i86yy

--- js/init.js
+++ js/init.js
@@ -6,29 +6,30 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-function validar(){
-	usuario = cadastro.usuario.value;
-	senha = cadastro.password.value;
-	confSenha = cadastro.passwordConf.value;
-	rasaoSocial = cadastro.rasaoSocial.value;
-	cnpj = cadastro.cnpj.value;
-	rua = cadastro.rua.value;
-	numeroCasa = cadastro.numeroCasa.value;
-	bairro = cadastro.bairro.value;
-	cidade = cadastro.cidade.value;
-	telefone = cadastro.telefone.value;
-	uf = cadastro.uf.value;
+usuario = cadastro.usuario.value;
+senha = cadastro.password.value;
+confSenha = cadastro.passwordConf.value;
+rasaoSocial = cadastro.rasaoSocial.value;
+cnpj = cadastro.cnpj.value;
+rua = cadastro.rua.value;
+numeroCasa = cadastro.numeroCasa.value;
+bairro = cadastro.bairro.value;
+cidade = cadastro.cidade.value;
+telefone = cadastro.telefone.value;
+uf = cadastro.uf.value;
 
+function validar(){
 	if (verificarVazio(usuario) || verificarVazio(senha) || verificarVazio(confSenha)
 	 || verificarVazio(rasaoSocial) || verificarVazio(cnpj) || verificarVazio(rua)
 	 || verificarVazio(numeroCasa) || verificarVazio(bairro) || verificarVazio(cidade)
 	 || verificarVazio(telefone) || verificarVazio(uf)){
 		alert('Todos os capos com "*" devem ser preenchidos.')
 	}
-
-	if (senha != confSenha){
-		alert('Senhas Diferentes!')
+	verificarSenha();
+	if (!validarCNPJ(cnpj);) {
+		alert('CNPJ invalido')
 	}
+	
 }
 
 function verificarVazio(campo){
@@ -36,4 +37,64 @@ function verificarVazio(campo){
 		return true;
 	}
 	return false;
+}
+
+function verificarSenha(){
+	if (senha != confSenha){
+		alert('Senhas Diferentes!')
+	}
+}
+
+function validarCNPJ(cnpj) {
+ 
+    cnpj = cnpj.replace(/[^\d]+/g,'');
+ 
+    if(cnpj == '') return false;
+     
+    if (cnpj.length != 14)
+        return false;
+ 
+    // Elimina CNPJs invalidos conhecidos
+    if (cnpj == "00000000000000" || 
+        cnpj == "11111111111111" || 
+        cnpj == "22222222222222" || 
+        cnpj == "33333333333333" || 
+        cnpj == "44444444444444" || 
+        cnpj == "55555555555555" || 
+        cnpj == "66666666666666" || 
+        cnpj == "77777777777777" || 
+        cnpj == "88888888888888" || 
+        cnpj == "99999999999999")
+        return false;
+         
+    // Valida DVs
+    tamanho = cnpj.length - 2
+    numeros = cnpj.substring(0,tamanho);
+    digitos = cnpj.substring(tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2)
+            pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(0))
+        return false;
+         
+    tamanho = tamanho + 1;
+    numeros = cnpj.substring(0,tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2)
+            pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(1))
+          return false;
+           
+    return true;
+    
 }
